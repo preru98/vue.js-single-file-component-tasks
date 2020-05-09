4954da93a328129b1604e9c987177ca6a4727587
Prerna <prerna1998.ps@gmail.com>
2020-05-09 23:57:33 +0530

    Login component

---
+++ login/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <login-user></login-user>
+  </div>
+</template>
+
+<script>
+import login from './components/login.vue'
+
+export default {
+  components:{
+  'login-user':login
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ login/src/components/sign.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="sign-up">
+    <form v-on:submit.prevent="onSubmitLogin">
+        <fieldset>
+            <legend>Login</legend>
+
+            <label for="email">Email</label><br>
+            <input v-model="email" type="email" id="email" name="email" required><br>
+
+            <label for="password">Password</label><br>
+            <input v-model="password" type="password" id="password" name="password" required><br>
+
+            <button>Sign up</button>
+            <input type="submit" value="LOG IN">
+        </fieldset> 
+    </form>  
+  </div>
+
+</template>
+
+<script>
+    export default {
+        data:function(){
+            return{
+                email:null,
+                password:null   
+            }
+        },
+        methods:{
+            onSubmitLogin:function(){
+                alert('login successful'+ this.email)
+                this.email=null
+                this.password=null
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
