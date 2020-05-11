4994b0aa302c64ef840e833539da2c090f490882
Prerna <prerna1998.ps@gmail.com>
2020-05-11 08:13:08 +0530

    Programmatic Navigation

--- router/src/views/Contact.vue
+++ router/src/views/Contact.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="contact">
     <p>Contact : prerna1998.ps@gmail.com</p>
+    <button v-on:click="back">Back To Home</button>
   </div>
 </template>
 
 <script>
-
+export default {
+    methods:{
+        back:function(){
+            this.$router.push('/')
+        }
+    }
+}
 </script>
